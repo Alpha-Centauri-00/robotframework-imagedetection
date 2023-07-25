e4e08776941610a09514afe9e00bad3b6806e896
alpha <alpha.com>
2023-07-25 10:43:52 +0200

    change license in setup

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(
     install_requires=_required_packages,
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: Apache-2.0 License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Topic :: Software Development :: Testing",
         "Topic :: Software Development :: Testing :: Acceptance",
