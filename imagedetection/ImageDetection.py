ce51f22d0db59496f32b6a01f43488d738a8b37c
alpha <alpha.com>
2023-07-26 08:56:26 +0200

    adding_classes

---
+++ imagedetection/ImageDetection.py
@@ -0,0 +1,13 @@
+from .Base_detection import Base_Detect
+from robot.api.deco import keyword,library
+from robot.api import logger
+
+ROBOT_LIBRARY_SCOPE = 'SUITE'
+
+class ImageDetection:
+    def __init__(self):
+        self.Base = Base_Detect()
+
+    @keyword
+    def Train_Model(self,training_path,validation_path):
+        self.Base.train_model(training_path,validation_path)
