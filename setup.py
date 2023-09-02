25357d05aeff964cd657024f7b296911a268694f
M.Kherki <56655042+Alpha-Centauri-00@users.noreply.github.com>
2023-09-02 09:38:05 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,6 @@
+import re
 from setuptools import setup, find_packages
+from os.path import abspath, dirname, join
 
 DESCRIPTION = "Image detection for Robot-Framework"
 LONG_DESCRIPTION = """
@@ -10,19 +12,30 @@ Key Features:
 - Simplify Robot Framework test automation with enhanced image recognition
 
 Take your Robot Framework tests to the next level with our image detection package.
+
 """
+
+CURDIR = dirname(abspath(__file__))
+with open("README.md", "r", encoding="utf-8") as fh:
+    Long_desc = LONG_DESCRIPTION + fh.read()
+
+with open(join(CURDIR, "ImageDetection", "Base_detection.py"), encoding="utf-8") as f:
+    VERSION = re.search(r'\n__version__ = "(.*)"', f.read()).group(1)
+
+with open("requirements.txt") as f:
+    _required_packages = f.read().splitlines()
 
 setup(
     name="robotframework-imagedetection",
-    version="1.0.0",  # Replace with your desired version number
+    version=VERSION,
     author="M.Kherki(Alpha-Centauri-00)",
     author_email="alpha_Centauri@posteo.de",
     description=DESCRIPTION,
-    long_description=LONG_DESCRIPTION,
+    long_description=Long_desc,
     long_description_content_type="text/markdown",
     url="https://github.com/Alpha-Centauri-00/robotframework-imagedetection",
     packages=find_packages(),
-    install_requires=[],  # Add your dependencies here
+    install_requires=_required_packages,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: Apache Software License",
