ee9afa701b196725c9941518b05f8ddc0cba6182
Alpha_Centauri <me@company.com>
2023-09-02 10:17:39 +0200

    edit_setup_again

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     long_description=Long_desc,
     long_description_content_type="text/markdown",
     url="https://github.com/Alpha-Centauri-00/robotframework-imagedetection",
-    packages=["Imagedetection"],
+    packages=["Imagedetection", "Imagedetection.ImageDetection", "Imagedetection.Base_detection"],
     install_requires=_required_packages,
     classifiers=[
         "Programming Language :: Python :: 3",
